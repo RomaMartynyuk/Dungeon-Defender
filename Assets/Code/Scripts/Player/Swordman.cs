2ef951f3d0f47fa4b7873661b4b03f496aeb6280
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2023-07-17 21:12:06 +0300

    Added Dash System

--- Assets/Code/Scripts/Player/Swordman.cs
+++ Assets/Code/Scripts/Player/Swordman.cs
@@ -15,6 +15,13 @@ public class Swordman : MonoBehaviour
     [SerializeField] GameObject gameOverUI;
     int health;
 
+    [Header("Dash")]
+    [SerializeField] float dashingPower = 10f;
+    [SerializeField] float dashingTime = 0.2f;
+    [SerializeField] float dashingCooldown = 1f;
+    bool canDash = true;
+    bool isDashing;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -24,18 +31,36 @@ public class Swordman : MonoBehaviour
 
     void Update()
     {
+        if (isDashing)
+            return;
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+            StopCoroutine(Dash());
+        }
     }
 
     void FixedUpdate()
     {
+        if (isDashing)
+            return;
+
+        Move();
+    }
+
+    //MOVEMENT
+    void Move()
+    {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        if(movement.x < 0 && facingRight)
+        if (movement.x < 0 && facingRight)
         {
             Flip();
         }
-        else if(movement.x > 0 && !facingRight)
+        else if (movement.x > 0 && !facingRight)
         {
             Flip();
         }
@@ -47,6 +72,7 @@ public class Swordman : MonoBehaviour
         transform.Rotate(0f, 180f, 0f);
     }
 
+    //HEALTH AND DIE
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -60,5 +86,31 @@ public class Swordman : MonoBehaviour
     {
         gameOverUI.SetActive(true);
         Destroy(gameObject);
+    }
+
+    //DASH
+    IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+
+        if (movement.y > 0)
+            rb.velocity = new Vector2(0f, transform.localScale.y * dashingPower);
+        else if (movement.y < 0)
+            rb.velocity = new Vector2(0f, -transform.localScale.y * dashingPower);
+        else if (facingRight)
+            rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        else if (!facingRight)
+            rb.velocity = new Vector2(-transform.localScale.x * dashingPower, 0f);
+
+        yield return new WaitForSeconds(dashingTime);
+
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+
+        canDash = true;
     }
 }
